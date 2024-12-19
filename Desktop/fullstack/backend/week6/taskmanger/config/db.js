baed8ffa2f22c15139a7c25357a914df42d4efdd
EFFY <157524653+POPCORNaa@users.noreply.github.com>
2024-12-19 03:49:35 +0200

    Update db.js

--- Desktop/fullstack/backend/week6/taskmanger/config/db.js
+++ Desktop/fullstack/backend/week6/taskmanger/config/db.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+
+// Connect to the database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected!');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
 
